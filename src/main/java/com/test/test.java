8b4ce51c33998d9243ab17d466d6e8a26f78f765
yupp <yupp@qq.com>
2022-01-19 16:52:57 +0800

    3 commit

--- src/main/java/com/test/test.java
+++ src/main/java/com/test/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello word1");
+        System.out.println("hello word2");
     }
 }
